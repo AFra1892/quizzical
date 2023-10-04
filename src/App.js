c6032acd7ff3227df23033be7d7452ddf709d92a
amir <mashhadifarahani1@gmail.com>
2023-10-04 17:35:48 +0330

    fetch an api and mapping that data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect,useState } from 'react';
+
+
 
 function App() {
+  const url = "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple";
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d.results))
+  }
+
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <center>
+        {data.map((dataObj, index) => {
+          return (
+            <div>
+              <p>{dataObj.question}</p>
+            </div>
+          );
+        })}
+      </center>
     </div>
   );
 }
